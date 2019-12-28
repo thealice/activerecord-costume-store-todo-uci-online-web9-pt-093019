47c792b0072383a53da483b26512f19bfa61a435
Alice Freda <alicefreda@gmail.com>
2019-12-28 01:11:30 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -5,7 +5,7 @@ class CreateCostumeStores < ActiveRecord::Migration[4.2]
       t.string :location
       t.integer :costume_inventory
       t.integer :num_of_employees
-      t.boolean :in_business?
+      t.boolean :still_in_business
       t.timestamp :opening
       t.timestamp :closing
     end
