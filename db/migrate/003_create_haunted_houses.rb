a3c29762258ace980b60e968d5742a4d655df577
Alice Freda <alicefreda@gmail.com>
2019-12-28 01:15:35 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,13 +1,14 @@
 class CreateHauntedHouses < ActiveRecord::Migration[4.2]
   def change
-    create_table :costume_stores do |t|
+    create_table :haunted_houses do |t|
       t.string :name
       t.string :location
-      t.integer :costume_inventory
-      t.integer :num_of_employees
-      t.boolean :still_in_business
-      t.timestamp :opening_time
-      t.timestamp :closing_time
+      t.string :theme
+      t.integer :price
+      t.boolean :family_friendly
+      t.timestamp :opening_date
+      t.timestamp :closing_date
+      t.text :description
     end
   end
 end
