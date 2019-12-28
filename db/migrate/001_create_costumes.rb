b7ff479afbc893d734f56b2e51c9dd7245e29cbf
Alice Freda <alicefreda@gmail.com>
2019-12-28 01:00:02 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -4,7 +4,7 @@ class CreateCostumes < ActiveRecord::Migration[4.2]
       t.string :name
       t.integer :price
       t.integer :size
-      t.url :image_url
+      t.string :image_url
       t.timestamps
     end
   end
